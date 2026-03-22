e1d9c654149c74d8e33693e98ee157912b4b06b9
romleiaj <adam.romlein@kitware.com>
2026-03-22 10:13:25 -0400

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,1 @@
+import { MetadataRoute } from 'next'export default function sitemap(): MetadataRoute.Sitemap {  return [    {      url: 'https://www.adamromlein.com',      lastModified: new Date(),      changeFrequency: 'monthly',      priority: 1,    },  ]}
